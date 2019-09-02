9fd48bc80f1f1862f22169cc75a88fc043f47780
Neumeier <neumeierj@media-saturn.com>
2019-09-02 12:57:59 +0200

    memory game with jasmine done from Johannes MediaMarktSaturn

--- starter-code/js/memory.js
+++ starter-code/js/memory.js
@@ -1,9 +1,45 @@
 class MemoryGame {
-  constructor(cards){
+  constructor(cards, pickedCards, pairsClicked, pairsGuessed){
     this.cards = cards;
     // add the rest of the class properties here
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
   }
-  shuffleCards() {}
-  checkIfPair(card1, card2) {}
-  isFinished() {}
+  shuffleCards(cards) {
+
+     let currentIndex = this.cards.length, temporaryValue, randomIndex;
+    
+     while (currentIndex !== 0) {
+       randomIndex = Math.floor(Math.random() * currentIndex);
+       currentIndex -= 1;
+       temporaryValue = this.cards[currentIndex];
+       this.cards[currentIndex] = this.cards[randomIndex];
+       this.cards[randomIndex] = temporaryValue;
+     }
+
+     return cards;
+
+  }
+
+
+  checkIfPair(card1, card2) {
+    this.pairsClicked += 1;
+    if (card1 === card2) {
+      this.pairsGuessed += 1;
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  isFinished() {
+    if (this.pairsGuessed *2 === this.cards.length) {
+      return true;
+    } else {
+      return false;
+    }
+
+  }
+
 }
